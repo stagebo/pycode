af8f28fd02a29f3c4f05571a40186b65a14fbb2c
stagebo <1254117589@qq.com>
2018-10-06 21:20:03 +0800

    commit by wyb at pycharm.

--- code_test/yield_test.py
+++ code_test/yield_test.py
@@ -7,17 +7,19 @@
     Description:   yield 使用测试
 """
 
-def fab(max_n):
+def fab():
     n,a,b = 0,0,1
-    while b < max_n:
+    while True:
         m = yield a
-        print(m)
         a,b,n = b,a+b,n+1
 if __name__ == "__main__":
-    x = fab(10)
+    x = fab()
     print(dir(x))
-    print(x.__next__())
-    # print(x.send(8))
-    print(x.__next__())
-    print(x.__next__())
-    print(x.__next__())
+    # print(x.__next__())
+    # # print(x.send(8))
+    # print(x.__next__())
+    # print(x.__next__())
+    # print(x.__next__())
+    print(next(x))
+    # for i in range(10000):
+    #     print(i,x.__next__())
