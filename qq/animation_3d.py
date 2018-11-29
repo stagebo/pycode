abe2a4d92c78fce151e8d19cd6438549af91a0e8
WYB <1254117589@qq.com>
2018-11-29 09:19:33 +0800

    commit by wyb at pycharm.

--- qq/animation_3d.py
+++ qq/animation_3d.py
@@ -7,7 +7,7 @@
     Description:   
 """
 import os, sys
-
+import copy
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -16,30 +16,66 @@ from matplotlib import cm
 fig = plt.figure(1)
 ax = fig.add_subplot(1, 1, 1, projection='3d')  # 指定三维空间做图
 
-t = np.linspace(0, 4, 200)  # 在0到4之间，均匀产生200点的数组
-theta = t * 2 * np.pi  # 角度
-
-# 生成曲线数组
-z = t
-x = np.sin(theta)
-y = np.cos(theta)
-
-# 运动的点
-# point, = ax.plot([x[0]], [y[0]], [z[0]], 'ro', label='p')
+# 训练数据集
 training_set = [[(1, 0, 0), 1],
-                [(0, 1, 1), 0],
                 [(1, 1, 0), 1],
-                [(1, 1, 1), 0],
-                [(0, 0, 1), 0],
-                [(1, 0, 1), 1]]
-ppx,ppy,pnx,pny = [],[],[],[]
-for t in training_set:
-    p = t[0]
-    lx = 'ro' if (t[1]==1) else 'rx'
-    ax.plot([p[0]],[p[1]],[p[2]],lx,label='p')
-# plt.plot(x, y, 'bo', x_, y_, 'rx')
-# 曲线
-line, = ax.plot([x[0]], [y[0]], [z[0]], label='line')
+                [(1, 0, 1), 1],
+                [(0, 1, 1), -1],
+                [(1, 1, 1), -1],
+                [(0, 0, 1), -1],
+                ]
+
+w = [0, 0, 0]  # 参数初始化
+b = 0
+# 超平面方程： w[0]*X + w[1]*Y + w[2] *Z + b =1
+history = []  # 用来记录每次更新过后的w,b
+
+
+def update(item):
+    """
+    随机梯度下降更新参数
+    :param item: 参数是分类错误的点
+    :return: nothing 无返回值
+    """
+    global w, b, history  # 把w, b, history声明为全局变量
+    w[0] += 0.1 * item[1] * item[0][0]  # 根据误分类点更新参数,这里学习效率设为1
+    w[1] += 0.1 * item[1] * item[0][1]
+    w[2] += 0.1 * item[1] * item[0][2]
+    b += 1 * item[1]
+    history.append([copy.copy(w), b])  # 将每次更新过后的w,b记录在history数组中
+
+
+def cal(item):
+    """
+    计算item到超平面的距离,输出yi(w*xi+b)
+    zi(w1*xi+w2*yi+b)
+    （我们要根据这个结果来判断一个点是否被分类错了。如果yi(w*xi+b)>0,则分类错了）
+    :param item:
+    :return:
+    """
+    # item = ((1,2),1)
+    res = 0
+    for i in range(len(item[0])):  # 迭代item的每个坐标，对于本文数据则有两个坐标x1和x2
+        res += item[0][i] * w[i]
+    res += b
+    res *= item[1]  # 这里是乘以公式中的yi
+    return res
+
+
+def check():
+    """
+    检查超平面是否已将样本正确分类
+    :return: true如果已正确分类则返回True
+    """
+    flag = False
+    for item in training_set:
+        if cal(item) <= 0:  # 如果有分类错误的
+            flag = True  # 将flag设为True
+            update(item)  # 用误分类点更新参数
+    print("中途: w: " + str(w) + "b: " + str(b))
+    if not flag:  # 如果没有分类错误的点了
+        print("最终结果: w: " + str(w) + "b: " + str(b))  # 输出达到正确结果时参数的值
+    return flag  # 如果已正确分类则返回True,否则返回False
 
 # 设置显示的范围和描述
 x_min = 0
@@ -61,25 +97,27 @@ ax.view_init(30, 35)
 # 设置标签在右下角
 ax.legend(loc='lower right')
 
-history = [[[0.1, 0.0, 0.0], 1], [[0.1, -0.1, -0.1], 0], [[0.2, 0.0, -0.1], 1], [[0.2, -0.1, -0.2], 0], [[0.30000000000000004, -0.1, -0.1], 1], [[0.30000000000000004, -0.2, -0.2], 0]]
+# history = [[[0.1, 0.0, 0.0], 1], [[0.1, -0.1, -0.1], 0], [[0.2, 0.0, -0.1], 1], [[0.2, -0.1, -0.2], 0], [[0.30000000000000004, -0.1, -0.1], 1], [[0.30000000000000004, -0.2, -0.2], 0]]
 
 
-w, b = [1.3, -1.3, -1.0999999999999999], 1
-X = np.arange(0, 1, 0.1)
-Y = np.arange(0, 1, 0.1)
-X, Y = np.meshgrid(X, Y)
-Z = -(w[0] * X + w[1] * Y + b) / w[2]
-# plain = ax.plot(X,Y,Z,label='plain')
-surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet, linewidth=0, antialiased=True)
-print('\n'.join(dir(surf)))
+surf = None
+def init():
+    global training_set,surf
+    for t in training_set:
+        p = t[0]
+        lx = 'ro' if (t[1] == 1) else 'rx'
+        ax.plot([p[0]], [p[1]], [p[2]], lx, label='p')
+
+    w, b = [1.3, -1.3, -1.0999999999999999], 1
+    w, b = [0, 0, -1], 1
+    X = np.arange(-1,2, 0.1)
+    Y = np.arange(-1,2, 0.1)
+    X, Y = np.meshgrid(X, Y)
+    Z = -(w[0] * X + w[1] * Y + b) / w[2]
+    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet, linewidth=0, antialiased=True)
+
 def animate(i):
     global  surf
-    # line.set_xdata(x[:i + 1])
-    # line.set_ydata(y[:i + 1])
-    # line.set_3d_properties(z[:i + 1])
-    # point.set_xdata(x[i])
-    # point.set_ydata(y[i])
-    # point.set_3d_properties(z[i])
     if i >= len(history):
         return
     w, b = history[i]
@@ -88,11 +126,17 @@ def animate(i):
     X, Y = np.meshgrid(X, Y)
     Z = -(w[0] * X + w[1] * Y + b) / w[2]
     # surf.update()
-    surf.remove()
+    if "remove" in dir(surf):
+        surf.remove()
     surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet, linewidth=0, antialiased=True)
 
 
+if __name__ == "__main__":
+    for i in range(100000):  # 迭代1000遍
+        if not check(): break  # 如果已正确分类，则结束迭代
+
 ani = animation.FuncAnimation(fig=fig,
+                              init_func=init,
                               func=animate,
                               frames=len(history),
                               interval=1000,
